1f775559fe90bf5143b66b9fb1566f4add4fc820
shehzad <ashr6574@gmail.com>
2024-08-02 16:05:33 +0500

    initial commit

---
+++ db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const mongoURL = "mongodb://127.0.0.1:27017";
+
+const connectToMongo = async () => {
+    try {
+        await mongoose.connect(mongoURL);
+        console.log("Connected to MongoDB Successfully...");
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+    }
+};
+
+module.exports = connectToMongo;
